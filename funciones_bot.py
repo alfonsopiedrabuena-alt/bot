ab45be7af070e7056a7f852ce2d63f45039b5d71
Alfonso Piedrabuena <alfonso.piedrabuena@tec.mx>
2020-12-06 13:39:39 -0600

    Cuarto commit, en este se trabajo la lógica de la tupla. Pendiente por recorrer la tupla, sumar un número y pasar por todas las opciones por medio de un if. Veremos la finalización del experimento en el próximo commit

--- funciones_bot.py
+++ funciones_bot.py
@@ -1,16 +1,4 @@
-from selenium import webdriver
 
-PATH = '/Applications/chromedriver'
-driver = webdriver.Chrome(PATH)
-
-driver.get('https://tec.mx/es/ambiente-construido')
-print(driver.title)
-#driver.close()
-
-nombre = '//*[@id="edit-nombre"]'
-apellido_paterno = '//*[@id="edit-apellido-paterno"]'
-apellido_materno = '//*[@id="edit-apellido-materno"]'
-telefono = '//*[@id="edit-telefono"]'
 mail_u = '//*[@id="edit-correo"]'
 dia_nac = '/html/body/div[1]/div/div[2]/div/div/article/div[2]/div/div[12]/div/div/div[2]/div[2]/form/fieldset/div[2]/fieldset[1]/fieldset[1]/div[1]/div'
 dia = '//*[@id="edit-birth"]/div[1]/div/div/div/ul/li[14]'
@@ -31,27 +19,28 @@ aviso = '//*[@id="edit-copy"]'
 submit = '//*[@id="edit-submit"]'
 
 
-driver.find_element_by_xpath(nombre).send_keys('Alfonsito')
-driver.find_element_by_xpath(apellido_paterno).send_keys('Piedrabuena')
-driver.find_element_by_xpath(apellido_materno).send_keys('Torrecillas')
-driver.find_element_by_xpath(telefono).send_keys('5539337086')
-driver.find_element_by_xpath(mail_u).send_keys('alfonso.piedrabuena@tec.mx')
-driver.find_element_by_xpath(dia_nac).click()
-driver.find_element_by_xpath(dia).click()
-driver.find_element_by_xpath(mes_nac).click()
-driver.find_element_by_xpath(mes).click()
-driver.find_element_by_xpath(ano_nac).click()
-driver.find_element_by_xpath(ano).click()
-driver.find_element_by_xpath(campus_opt).click()
-driver.find_element_by_xpath(campus).click()
-driver.find_element_by_xpath(fecha_opt).click()
-driver.find_element_by_xpath(fecha).click()
-driver.find_element_by_xpath(area_opt).click()
-driver.find_element_by_xpath(area).click()
-driver.find_element_by_xpath(carrera_opt).click()
-driver.find_element_by_xpath(carrera).click()
-driver.find_element_by_xpath(aviso_check).click()
-driver.find_element_by_xpath(submit).click()
+tupla = (mail_u,dia_nac,dia,mes_nac,mes,ano_nac,ano,campus_opt,campus,fecha_opt,fecha,area_opt,area,carrera_opt,carrera,aviso_check,aviso,submit)
+for i in tupla:
+    i = 0, i + 1, i=+
+    print (i)    
+
+driver.find_element_by_xpath(i).click()
+#driver.find_element_by_xpath(dia_nac).click()
+#driver.find_element_by_xpath(dia).click()
+#driver.find_element_by_xpath(mes_nac).click()
+#driver.find_element_by_xpath(mes).click()
+#driver.find_element_by_xpath(ano_nac).click()
+#driver.find_element_by_xpath(ano).click()
+#driver.find_element_by_xpath(campus_opt).click()
+#driver.find_element_by_xpath(campus).click()
+#driver.find_element_by_xpath(fecha_opt).click()
+#driver.find_element_by_xpath(fecha).click()
+#driver.find_element_by_xpath(area_opt).click()
+#driver.find_element_by_xpath(area).click()
+#driver.find_element_by_xpath(carrera_opt).click()
+#driver.find_element_by_xpath(carrera).click()
+#driver.find_element_by_xpath(aviso_check).click()
+#driver.find_element_by_xpath(submit).click()
 print('fin de la ejecución')
 
 driver.close()
