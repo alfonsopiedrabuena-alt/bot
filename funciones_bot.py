1c69bcd56cdcc65d3ed679ea44f12961448f1327
Alfonso Piedrabuena <alfonso.piedrabuena@tec.mx>
2020-12-06 23:29:57 -0600

    Septimo cCommit, todo funciona bien. Pendiente nuevamente la reestructura del código a clases y funciones.

--- funciones_bot.py
+++ funciones_bot.py
@@ -13,46 +13,46 @@ sitios = {
 for ligas in sitios.values():
     print (ligas)
     
-driver.get(ligas)
+    driver.get(ligas)
 
-print(driver.title)
+    print(driver.title)
 
-nombre = '//*[@id="edit-nombre"]'
-apellido_paterno = '//*[@id="edit-apellido-paterno"]'
-apellido_materno = '//*[@id="edit-apellido-materno"]'
-telefono = '//*[@id="edit-telefono"]'
-mail_u = '//*[@id="edit-correo"]'
-dia_nac = '//*[@id="edit-birth"]/div[1]/div/div/div'
-dia = '//*[@id="edit-birth"]/div[1]/div/div/div/ul/li[14]'
-mes_nac = '//*[@id="edit-birth"]/div[2]/div/div/div'
-mes = '//*[@id="edit-birth"]/div[2]/div/div/div/ul/li[3]'
-ano_nac = '//*[@id="edit-birth"]/div[3]/div/div/div'
-ano = '//*[@id="edit-birth"]/div[3]/div/div/div/ul/li[4]'
-campus_opt = '//*[@id="edit-block-3"]/div[1]/div/div'
-campus = '//*[@id="edit-block-3"]/div[1]/div/div/ul/li[5]'
-fecha_opt = '//*[@id="edit-block-3"]/div[2]/div/div'
-fecha = '//*[@id="edit-block-3"]/div[2]/div/div/ul/li[3]'
-area_opt = '//*[@id="edit-block-3"]/div[3]/div/div'
-area = '//*[@id="edit-block-3"]/div[3]/div/div/ul/li[5]'
-carrera_opt = '//*[@id="edit-block-3"]/div[4]/div/div'
-carrera = '//*[@id="edit-block-3"]/div[4]/div/div/ul/li[2]'
-aviso_check = '//*[@id="edit-copy"]'
-aviso = '//*[@id="edit-copy"]'
-submit = '//*[@id="edit-submit"]'
+    nombre = '//*[@id="edit-nombre"]'
+    apellido_paterno = '//*[@id="edit-apellido-paterno"]'
+    apellido_materno = '//*[@id="edit-apellido-materno"]'
+    telefono = '//*[@id="edit-telefono"]'
+    mail_u = '//*[@id="edit-correo"]'
+    dia_nac = '//*[@id="edit-birth"]/div[1]/div/div/div'
+    dia = '//*[@id="edit-birth"]/div[1]/div/div/div/ul/li[2]'
+    mes_nac = '//*[@id="edit-birth"]/div[2]/div/div/div'
+    mes = '//*[@id="edit-birth"]/div[2]/div/div/div/ul/li[3]'
+    ano_nac = '//*[@id="edit-birth"]/div[3]/div/div/div'
+    ano = '//*[@id="edit-birth"]/div[3]/div/div/div/ul/li[2]'
+    campus_opt = '//*[@id="edit-block-3"]/div[1]/div/div'
+    campus = '//*[@id="edit-block-3"]/div[1]/div/div/ul/li[5]'
+    fecha_opt = '//*[@id="edit-block-3"]/div[2]/div/div'
+    fecha = '//*[@id="edit-block-3"]/div[2]/div/div/ul/li[3]'
+    area_opt = '//*[@id="edit-block-3"]/div[3]/div/div'
+    area = '//*[@id="edit-block-3"]/div[3]/div/div/ul/li[5]'
+    carrera_opt = '//*[@id="edit-block-3"]/div[4]/div/div'
+    carrera = '//*[@id="edit-block-3"]/div[4]/div/div/ul/li[2]'
+    aviso_check = '//*[@id="edit-copy"]'
+    aviso = '//*[@id="edit-copy"]'
+    submit = '//*[@id="edit-submit"]'
 
-driver.find_element_by_xpath(nombre).send_keys('Alfonso')
-driver.find_element_by_xpath(apellido_paterno).send_keys('Piedrabuena')
-driver.find_element_by_xpath(apellido_materno).send_keys('Torrecillas')
-driver.find_element_by_xpath(telefono).send_keys('5539337086')
-driver.find_element_by_xpath(mail_u).send_keys('alfonso.piedrabuena@tec.mx')
+    driver.find_element_by_xpath(nombre).send_keys('Alfonso')
+    driver.find_element_by_xpath(apellido_paterno).send_keys('Piedrabuena')
+    driver.find_element_by_xpath(apellido_materno).send_keys('Torrecillas')
+    driver.find_element_by_xpath(telefono).send_keys('5539337086')
+    driver.find_element_by_xpath(mail_u).send_keys('alfonso.piedrabuena@tec.mx')
 
-lista = [dia_nac,dia,mes_nac,mes,ano_nac,ano,campus_opt,campus,fecha_opt,fecha,area_opt,area,carrera_opt,carrera,aviso_check,submit]
-for elementos in lista:
-    print(elementos)
-    driver.find_element_by_xpath(elementos).click()
-    
-print('fin de la ejecución')
+    lista = [dia_nac,dia,mes_nac,mes,ano_nac,ano,campus_opt,campus,fecha_opt,fecha,area_opt,area,carrera_opt,carrera,aviso_check,submit]
+    for elementos in lista:
+        print(elementos)
+        driver.find_element_by_xpath(elementos).click()
+
+    print('fin de la ejecución')
 
 
-print ('Todo corrió')
+    print ('Todo corrió')
 
