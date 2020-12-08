c244e4414bfa0424ea98db1e6df26c09af396f6d
Alfonso Piedrabuena <alfonso.piedrabuena@tec.mx>
2020-12-07 22:13:44 -0600

    10º Creando funciones
    Pendiente la generación de clases

--- funciones_bot.py
+++ funciones_bot.py
@@ -5,6 +5,8 @@ driver = webdriver.Chrome(PATH)
 
 def run():
     sitios_areas()
+    print ('Terminó')
+
 
 def sitios_areas():
 
@@ -23,45 +25,43 @@ def sitios_areas():
     for ligas in sitios.values():
         print (ligas)
         driver.get(ligas)
+        accion_bot()
 
         print(driver.title)
 
-        nombre = '//*[@id="edit-nombre"]'
-        apellido_paterno = '//*[@id="edit-apellido-paterno"]'
-        apellido_materno = '//*[@id="edit-apellido-materno"]'
-        telefono = '//*[@id="edit-telefono"]'
-        mail_u = '//*[@id="edit-correo"]'
-        dia_nac = '//*[@id="edit-birth"]/div[1]/div/div/div'
-        dia = '//*[@id="edit-birth"]/div[1]/div/div/div/ul/li[2]'
-        mes_nac = '//*[@id="edit-birth"]/div[2]/div/div/div'
-        mes = '//*[@id="edit-birth"]/div[2]/div/div/div/ul/li[2]'
-        ano_nac = '//*[@id="edit-birth"]/div[3]/div/div/div'
-        ano = '//*[@id="edit-birth"]/div[3]/div/div/div/ul/li[2]'
-        campus_opt = '//*[@id="edit-block-3"]/div[1]/div/div'
-        campus = '//*[@id="edit-block-3"]/div[1]/div/div/ul/li[5]'
-        fecha_opt = '//*[@id="edit-block-3"]/div[2]/div/div'
-        fecha = '//*[@id="edit-block-3"]/div[2]/div/div/ul/li[3]'
-        area_opt = '//*[@id="edit-block-3"]/div[3]/div/div'
-        area = '//*[@id="edit-block-3"]/div[3]/div/div/ul/li[5]'
-        carrera_opt = '//*[@id="edit-block-3"]/div[4]/div/div'
-        carrera = '//*[@id="edit-block-3"]/div[4]/div/div/ul/li[2]'
-        aviso_check = '//*[@id="edit-copy"]'
-        aviso = '//*[@id="edit-copy"]'
-        submit = '//*[@id="edit-submit"]'
-
-        driver.find_element_by_xpath(nombre).send_keys('Alfonso')
-        driver.find_element_by_xpath(apellido_paterno).send_keys('Piedrabuena')
-        driver.find_element_by_xpath(apellido_materno).send_keys('Torrecillas')
-        driver.find_element_by_xpath(telefono).send_keys('5539337086')
-        driver.find_element_by_xpath(mail_u).send_keys('alfonso.piedrabuena@tec.mx')
-
-        lista = [dia_nac,dia,mes_nac,mes,ano_nac,ano,campus_opt,campus,fecha_opt,fecha,area_opt,area,carrera_opt,carrera,aviso_check,submit]
-        for elementos in lista:
-            driver.find_element_by_xpath(elementos).click()
-
-        print('fin de la ejecución')
+def  accion_bot():
+    nombre = '//*[@id="edit-nombre"]'
+    apellido_paterno = '//*[@id="edit-apellido-paterno"]'
+    apellido_materno = '//*[@id="edit-apellido-materno"]'
+    telefono = '//*[@id="edit-telefono"]'
+    mail_u = '//*[@id="edit-correo"]'
+    dia_nac = '//*[@id="edit-birth"]/div[1]/div/div/div'
+    dia = '//*[@id="edit-birth"]/div[1]/div/div/div/ul/li[2]'
+    mes_nac = '//*[@id="edit-birth"]/div[2]/div/div/div'
+    mes = '//*[@id="edit-birth"]/div[2]/div/div/div/ul/li[2]'
+    ano_nac = '//*[@id="edit-birth"]/div[3]/div/div/div'
+    ano = '//*[@id="edit-birth"]/div[3]/div/div/div/ul/li[2]'
+    campus_opt = '//*[@id="edit-block-3"]/div[1]/div/div'
+    campus = '//*[@id="edit-block-3"]/div[1]/div/div/ul/li[5]'
+    fecha_opt = '//*[@id="edit-block-3"]/div[2]/div/div'
+    fecha = '//*[@id="edit-block-3"]/div[2]/div/div/ul/li[3]'
+    area_opt = '//*[@id="edit-block-3"]/div[3]/div/div'
+    area = '//*[@id="edit-block-3"]/div[3]/div/div/ul/li[5]'
+    carrera_opt = '//*[@id="edit-block-3"]/div[4]/div/div'
+    carrera = '//*[@id="edit-block-3"]/div[4]/div/div/ul/li[2]'
+    aviso_check = '//*[@id="edit-copy"]'
+    aviso = '//*[@id="edit-copy"]'
+    submit = '//*[@id="edit-submit"]'
+    driver.find_element_by_xpath(nombre).send_keys('Alfonso')
+    driver.find_element_by_xpath(apellido_paterno).send_keys('Piedrabuena')
+    driver.find_element_by_xpath(apellido_materno).send_keys('Torrecillas')
+    driver.find_element_by_xpath(telefono).send_keys('5539337086')
+    driver.find_element_by_xpath(mail_u).send_keys('alfonso.piedrabuena@tec.mx')
+    lista = [dia_nac,dia,mes_nac,mes,ano_nac,ano,campus_opt,campus,fecha_opt,fecha,area_opt,area,carrera_opt,carrera,aviso_check,submit]
+    for elementos in lista:
+        driver.find_element_by_xpath(elementos).click()
+    print('fin de la ejecución')
     
-print ('Terminó')
 
 if __name__ == '__main__':
     run()
